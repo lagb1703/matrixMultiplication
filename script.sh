92376c975f6091751c55a7d748bb97a8d6abcdab
lagb1703 <luis.giraldo3@gmail.com>
2025-09-15 00:42:51 +0000

    elimand  los csv

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ gcc ./libro.c -o libro.out
 gcc ./tradicional.c -o tradicional.out
 mkdir -p csv
 echo "0, 500,1000,1500,2000,2500,3000" >csv/libro.csv
-for z in 1 2 3 4 5 6 7 8 9 10; do
+for z in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
     for j in 4 8 12 16; do
         echo -n $j >>csv/libro.csv
         echo -n "," >>csv/libro.csv
-    for i in 500 1000 1500 2000 2500 3000; do
+    for i in 1000 1500 2000 2500 3000 3500 4000; do
         ./libro.out $i $j >>csv/libro.csv
-        if [ $i != 3000 ]; then
+        if [ $i != 4000 ]; then
             echo -n "," >>csv/libro.csv
         fi
     done
@@ -19,13 +19,13 @@ for z in 1 2 3 4 5 6 7 8 9 10; do
 done
 
 echo "0, 500,1000,1500,2000,2500,3000" >csv/tradicional.csv
-for z in 1 2 3 4 5 6 7 8 9 10; do
+for z in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
     for j in 4 8 12 16; do
         echo -n $j >>csv/tradicional.csv
         echo -n "," >>csv/tradicional.csv
-    for i in 500 1000 1500 2000 2500 3000; do
+    for i in 1000 1500 2000 2500 3000 3500 4000; do
         ./tradicional.out $i $j >>csv/tradicional.csv
-        if [ $i != 3000 ]; then
+        if [ $i != 4000 ]; then
             echo -n "," >>csv/tradicional.csv
         fi
     done
