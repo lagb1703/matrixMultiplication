e347727f5b32c8fed92beb4c9b681cb3f875741f
lagb1703 <luis.giraldo3@gmail.com>
2025-11-20 16:23:49 -0500

    broadcast

--- tradicional.c
+++ tradicional.c
@@ -75,7 +75,7 @@ void broadcastMatrix(MATRIX a, UI32 n)
     for (int i = 0; i < n; i++)
     {
         printf("Esto es una mala idea");
-        MPI_Bcast(a[i], n, MPI_INT, 0, MPI_COMM_WORLD);
+        // MPI_Bcast(a[i], n, MPI_INT, 0, MPI_COMM_WORLD);
     }
 }
 
